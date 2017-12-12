0f9f457edd0ef7f24fc8069417a0b054d0e3d7f0
julianfaul <julianfaul3@gmail.com>
2017-12-12 15:44:16 +0200

    first commit

---
+++ src/assets/vue/pages/testpage.vue
@@ -0,0 +1,12 @@
+<template>
+	<f7-page>
+		<f7-navbar title="My Page" back-link="Back" sliding></f7-navbar>
+		<f7-block inner>
+		<h1>MY 1 page</h1>
+		</f7-block>
+	</f7-page>
+</template>
+
+<script>
+	export default {}
+</script>
